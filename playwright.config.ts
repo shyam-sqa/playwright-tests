c460ac09b895f411bf6c3de1177d775915bd07e5
Shyam k <kotechashyam1819@gmail.com>
2026-04-13 12:11:10 +0530

    updated CI config

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,3 +1,4 @@
+/// <reference types="node" />
 import { defineConfig, devices } from '@playwright/test';
 
 /**
@@ -18,10 +19,10 @@ export default defineConfig({
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  //retries: process.env.CI ? 2 : 0,
-  retries: 3,
+  retries: process.env.CI ? 2 : 0,
+  //retries: 3,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  workers: process.env.CI ? 2 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
