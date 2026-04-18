907a34521c70509f777e1538d96c78c64da8334f
Shyam k <kotechashyam1819@gmail.com>
2026-04-18 22:26:34 +0530

    feat: implement data-driven Slack notifications and Allure report integration

--- playwright.config.ts
+++ playwright.config.ts
@@ -24,14 +24,20 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 2 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [
+    ['html'],
+    ['json', { outputFile: 'results.json' }],
+    ['allure-playwright',{ outputFolder: 'allure-results' }]
+  ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
     // baseURL: 'http://localhost:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    screenshot: 'only-on-failure',
+    video: 'retain-on-failure',
+    trace: 'on-first-retry',    
   },
 
   /* Configure projects for major browsers */
@@ -40,7 +46,7 @@ export default defineConfig({
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
     },
-
+    /*
     {
       name: 'firefox',
       use: { ...devices['Desktop Firefox'] },
@@ -50,7 +56,7 @@ export default defineConfig({
       name: 'webkit',
       use: { ...devices['Desktop Safari'] },
     }
-
+    */
     /* Test against mobile viewports. */
     // {
     //   name: 'Mobile Chrome',
